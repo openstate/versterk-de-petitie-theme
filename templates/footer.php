aec8dfabad08374e3c2752bbbc4e405de7b1a5ba
Sicco van Sas <siccovansas@users.noreply.github.com>
2018-01-16 19:27:25 +0100

    Update footer

--- templates/footer.php
+++ templates/footer.php
@@ -12,16 +12,7 @@
       </div>
       <br>
       <div>
-        <a href="http://www.rekenkamer.nl/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/ar_bw.svg'; ?>" alt="Algemene Rekenkamer logo"></a>
-        <a href="https://www.tweedekamer.nl/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/tk_bw.svg'; ?>" alt="Tweede Kamer logo"></a>
         <a href="https://openstate.eu/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/logo-open-state-black.svg'; ?>" alt="Open State Foundation logo"></a>
-        <a href="https://www.cbs.nl/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/cbs_bw.svg'; ?>" alt="Centraal Bureau voor de Statistiek logo"></a>
-        <br>
-        <br>
-        <a href="https://www.rijksoverheid.nl/ministeries/ministerie-van-binnenlandse-zaken-en-koninkrijksrelaties" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/minbzk_bw.svg'; ?>" alt="Ministerie van Binnenlandse Zaken en Koninkrijksrelaties logo"></a>
-        <a href="https://www.rijksoverheid.nl/ministeries/ministerie-van-infrastructuur-en-milieu" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/minienm_bw.svg'; ?>" alt="Ministerie van Infrastructuur en Milieu logo"></a>
-        <a href="https://www.rijksoverheid.nl/ministeries/ministerie-van-buitenlandse-zaken" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/minbuza_bw.svg'; ?>" alt="Ministerie van Buitenlandse Zaken logo"></a>
-        <a href="https://www.rijksoverheid.nl/ministeries/ministerie-van-financien" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/minfin_bw.svg'; ?>" alt="Ministerie van Financiën logo"></a>
       </div>
     </div>
     <div class="visible-xs-block">
