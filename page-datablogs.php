fe66f132a2c08b113997b82ffcc22fcd1bc844ea
nora <osfnora@users.noreply.github.com>
2018-03-29 19:05:33 +0200

    tekst en plaatjes

---
+++ page-datablogs.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Datablogs Page
+ */
+?>
+<div class="archive">
+  <div class="row">
+    <br>
+    <hr style="background-color: white; height: 18px; margin: 0;">
+
+    <div class="col-md-offset-2">
+      <h1 class="post-header">Datablogs</h1>
+      <div class="lijntje-div lijntje-grijs">
+        <?php
+          $args = array( 'posts_per_page' => 100, 'category_name' => 'Datablog' );
+          $myposts = get_posts( $args );
+          foreach ( $myposts as $post ) : setup_postdata( $post );
+        ?>
+          <?php get_template_part('templates/content', 'post'); ?>
+        <?php endforeach;
+          wp_reset_postdata();
+        ?>
+      </div>
+    </div>
+  </div>
+</div>
--- templates/frontpage-content.php
+++ templates/frontpage-content.php
@@ -14,11 +14,29 @@
       </div>
       <div class="col-xs-12 col-md-6">
         <p class="frontpage-content-text">
-          Welkom bij Hack de Valse Start. De hackathon die zich richt op kansen(on)gelijkheid in het onderwijs in Amsterdam. Gelijke kansen in het Nederlandse onderwijs staan onder druk. Met gebruik van open data gaan we op zoek naar nieuwe manieren om kansenongelijkheid te signaleren en gelijke kansen te verbeteren.
+          <h1>Digitale Indiana Jones gezocht! Doe mee met de app challenge</h1>
+          <h4>De provincie Zuid-Holland is een rijk archeologisch gebied. Archeologie vertelt veel over onze geschiedenis. De provincie Zuid-Holland wil samen met anderen erfgoed helpen ontdekken, beschermen en zorgen dat iedereen er kennis mee kan maken. Archeologie is daarvan een mooi voorbeeld. <br><br>
+
+          De provincie Zuid-Holland stelt haar archeologische objecten, zoals Romeinse munten, 3D schalen en opgravingsdocumentatie, beschikbaar als open data. Hiermee zijn duizenden objecten, afbeeldingen en documenten beschikbaar voor hergebruik. De provincie Zuid-Holland daagt jou daarom uit om deze digitale schatkamer te hergebruiken tijdens de Digging for Data app challenge. Het winnende idee ontvangt van de provincie Zuid-Holland €20.000 ontwikkelbudget om de app daadwerkelijk te kunnen ontwikkelen en bouwen! <br><br>
+
+          Tijdens de Digging for Data app challenge worden ontwikkelaars uitgenodigd om een uitgewerkt app-idee in te sturen. Het idee moet als uitgangspunt hebben om het publieksbereik van archeologische vondsten te vergroten. Archeologie moet fascineren, inspireren en de applicatie moet natuurlijk ook een educatieve meerwaarde hebben door gebruikers van de app kennis te laten maken met de unieke archeologische stukken van de provincie Zuid-Holland. <br><br>
+
+          Heb jij affiniteit met zowel archeologie als datawetenschap en informatica? Heb jij een fantastisch idee voor een archeologie applicatie en kan jij het ook daadwerkelijk uitvoeren? Kortom, ben jij Indiana Jones 2.0 en wil jij met jouw ‘ontdekking’ kans maken op €20.000 ontwikkelbudget? Kom dan 20 april 2018 naar de kick-off meeting bij het COMM Museum in Den Haag en kom alles te weten over de app challenge. </h4>
         </p>
         <div class="align-center">
-          <span><a href="https://www.eventbrite.nl/e/tickets-hack-de-valse-start-42635554090" class="btn btn-registration" role="button">Meld je aan</a></span>
-        </div>
+          <span><a href="https://docs.google.com/forms/d/e/1FAIpQLSfbk0gRlvv_ihfB3_aJpoNBAgwJn8cq4PzUuyuN6knmSYlvXA/viewform?usp=sf_link" class="btn btn-registration" role="button">Meld je aan</a></span> <br><br>
+        </div> 
+
+          Na de kick-off kan iedereen zijn of haar app-idee inzenden tot en met 13 juni. <br><br>
+
+          Details kick-off meeting:<br>
+          Datum: vrijdag 20 april 2018<br>
+          Tijd: 12.30 - 15.30 event<br>
+          15.30 - 17.30 borrel<br>
+          Locatie: COMM Museum <br>
+          Zeestraat 82<br>
+          2518 AD Den Haag<br><br>
+          Tijdens de kick-off meeting krijg je meer informatie over de databronnen en de app challenge. Na de presentaties is er een brainstormsessie en een borrel om de creativiteit met een hapje en een drankje aan te wakkeren. Kijk voor het volledige programma onder het kopje ‘app challenge’. <br><br>
       </div>
      <div class="col-xs-12 col-md-3 nieuwsberichtenplaatje"> 
       <img class="img-responsive block" src="<?= get_template_directory_uri() . '/dist/images/welkomplaatje.jpg' ?>" alt="Gemstone with a carving of two horses by a trough, Roman period. Foto van de Provincie Zuid-Holland">
