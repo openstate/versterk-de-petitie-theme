c4437c397c4bfeffbae8731972e5d53c758fa67a
Sicco van Sas <siccovansas@users.noreply.github.com>
2018-05-01 18:41:58 +0200

    Some more changes

--- templates/head.php
+++ templates/head.php
@@ -5,20 +5,20 @@
   <meta name="author" content="Open State Foundation">
   <meta property="og:title" content="Versterk de Petitie - 5 oktober 2018 @ DesignLab (Universiteit Twente)" />
   <meta property="og:type" content="website" />
-  <meta property="og:image" content="https://hackdevalsestart.nl/wp-content/themes/hack-de-valse-start-theme/dist/images/logo_hack_de_valse_start_og.png" />
-  <meta property="og:image:secure_url" content="https://hackdevalsestart.nl/wp-content/themes/hack-de-valse-start-theme/dist/images/logo_hack_de_valse_start_og.png" />
+  <meta property="og:image" content="https://versterkdepetitie.nl/wp-content/themes/versterk-de-petitie-theme/dist/images/logo-vds-og.png" />
+  <meta property="og:image:secure_url" content="https://versterkdepetitie.nl/wp-content/themes/versterk-de-petitie-theme/dist/images/logo-vds-og.png" />
   <meta property="og:image:type" content="image/png" />
   <meta property="og:image:width" content="1500" />
   <meta property="og:image:height" content="500" />
   <?php wp_head(); ?>
   <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
   <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-32274817-13"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-32274817-16"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'UA-32274817-13', { 'anonymize_ip': true });
-    gtag('config', 'UA-32274817-13');
+    gtag('config', 'UA-32274817-16', { 'anonymize_ip': true });
+    gtag('config', 'UA-32274817-16');
   </script>
 </head>
