1c2b4be390cf67c79a3531bb3dc745b8b39de8a9
Sicco van Sas <siccovansas@users.noreply.github.com>
2018-10-18 18:56:01 +0200

    Update frontpage after event

--- templates/frontpage-content.php
+++ templates/frontpage-content.php
@@ -8,18 +8,13 @@
   <div class="row">
     <div class="col-xs-12">
       <div class="balk bg-geel">
-        <center>Save the date!</center>
+        <center>Terugblik op de data-challenge</center>
       </div>
     </div>
   </div>
   <div class="row">
     <div class="col-xs-12">
-      <p class="frontpage-content-text">
-        Vind jij een vitale en inclusieve democratie van groot belang en heb je een passie voor data-analyses? Deel dan jouw kennis, netwerk en tijd en ga met technici, wetenschappers en journalisten op zoek naar mogelijkheden om de beleidsinvloed van petitionarissen te versterken. Wij zorgen voor een inspirerende setting, kundige deelnemers, en -uiteraard- enkele unieke datasets waaronder die van het grootste petities-platform in Nederland.</p>
-        <div class="align-center">
-        <span><a href="https://www.eventbrite.nl/e/tickets-versterk-de-petitie-45651197962" class="btn btn-registration" role="button"><b>Meld je aan</b></a></span>
-        <br>
-      </div>
+      <a href="https://versterkdepetitie.nl/2018/10/17/terugblik-op-de-data-challenge/"><img class="img-responsive center-block" src="<?= get_template_directory_uri() . '/dist/images/winnaar.png'; ?>" alt="Winnaar van Versterk de Petitie"></a>
        <p class="frontpage-content-text">
         <br>
         <b>Wanneer</b>: vrijdag 12 oktober 2018
